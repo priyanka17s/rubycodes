bc06b9fa2cba7bf5f9a4e66366ede45d731d7d3b
priyanka17s <apriyankasaravanakumar1707@gmail.com>
2025-10-07 20:58:29 +0530

    Add files via upload

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+desserts= Array["brownie", "cheesecake", "tiramisu", "chocolava"]
+puts desserts
+puts desserts[3]
+desserts [1]= "jamun"
+puts desserts.include?("tiramisu")
+puts desserts.include?("tirameeshu")
+puts desserts.reverse()
+puts desserts.sort()
