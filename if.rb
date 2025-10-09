94ea241216e38f9ec7691c8f392bf1aeb445b8fd
priyanka17s <apriyankasaravanakumar1707@gmail.com>
2025-10-09 23:40:25 +0530

    Add files via upload

---
+++ if.rb
@@ -0,0 +1,18 @@
+isage = false
+ifvote = false
+if isage and ifvote
+    puts "you are 18 years old and eligible to vote"
+else
+    puts "you are not 18 years old and not eligible to vote"
+end
+
+mark = 65
+if mark >= 60
+    puts "you have passed the exam"
+elsif mark >= 50
+    puts "you have passed the exam with a second class"
+elsif mark >= 40
+    puts "you have passed the exam with a third class"
+else
+    puts "you have failed the exam"
+end
