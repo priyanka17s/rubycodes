d46c27d0e5042051b41bf02934dd35aa13b7ec61
priyanka17s <apriyankasaravanakumar1707@gmail.com>
2025-10-09 23:41:42 +0530

    Update if.rb

--- if.rb
+++ if.rb
@@ -1,11 +1,3 @@
-isage = false
-ifvote = false
-if isage and ifvote
-    puts "you are 18 years old and eligible to vote"
-else
-    puts "you are not 18 years old and not eligible to vote"
-end
-
 mark = 65
 if mark >= 60
     puts "you have passed the exam"
@@ -16,3 +8,4 @@ elsif mark >= 40
 else
     puts "you have failed the exam"
 end
+
